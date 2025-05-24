1a644aaed98bd7a4a0b818af1827e5ae4ccf2feb
tacherasasi <tacherasasi@gmail.com>
2025-05-25 00:34:37 +0300

    Add server setup and configuration for Apollo Server

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './schema.js';
+
+const PORT = 3000
+
+
+const server = new ApolloServer({
+    typeDefs,
+
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: PORT },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+
